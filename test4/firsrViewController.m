8ba47791e92c15148157785b31f4f16af22e5f8b
mads <mads@sina.com>
2014-10-10 14:37:25 +0800

    dccdcd

--- test4/firsrViewController.m
+++ test4/firsrViewController.m
@@ -18,7 +18,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        // Custom initialization cdefefefefefe
     }
     return self;
 }
